ddf58351928cbbaa309262a61cf5a087edef213c
sasatani <sasayabaku@yahoo.co.jp>
2018-03-09 10:40:44 +0900

    [Clean] ELM_CNN_MNIST refactor and debug

--- elm_cnn_mnist.py
+++ elm_cnn_mnist.py
@@ -4,7 +4,6 @@ import hpelm
 from sklearn.datasets import load_digits
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import precision_recall_fscore_support
-from keras.utils import np_utils
 
 from keras.models import Sequential, Model
 from keras.layers.convolutional import Conv2D
@@ -12,8 +11,6 @@ from keras.layers.pooling import MaxPool2D
 from keras.optimizers import Adam
 
 from keras.layers.core import Dense, Activation, Flatten
-from keras.utils import plot_model
-from keras.callbacks import EarlyStopping
 
 from keras.utils import np_utils
 
@@ -21,8 +18,8 @@ import numpy as np
 import plotly
 import plotly.figure_factory as ff
 
-
 NUM_CLASS = 10
+CNN_EPOCH = 50
 ELM_HIDDEN_NEURONS = 1500
 
 
@@ -37,17 +34,16 @@ def load_mnist_2d():
     data_train_2d = np.reshape(data_train, (data_train.shape[0], 8, 8, 1))
     data_test_2d = np.reshape(data_test, (data_test.shape[0], 8, 8, 1))
 
-    target_train_oh = np_utils.to_categorical(target_train, NUM_CLASS)
-    target_test_oh = np_utils.to_categorical(target_test, NUM_CLASS)
-
     return data_train_2d, data_test_2d, target_train, target_test
 
 
-def cnn_generate(data_train_2d, target_train_oh):
+def cnn_generate(data_train_2d, target_train):
 
     """
     CNNモデルの構築
     """
+
+    target_train_oh = np_utils.to_categorical(target_train, NUM_CLASS)
 
     cnn_model = Sequential()
     cnn_model.add(Conv2D(20, 2, input_shape=(data_train_2d.shape[1], data_train_2d.shape[2], 1)))
@@ -66,7 +62,7 @@ def cnn_generate(data_train_2d, target_train_oh):
 
     cnn_model.compile(optimizer=adam, loss='categorical_crossentropy', metrics=['accuracy'])
 
-    history = cnn_model.fit(data_train_2d, target_train_oh, batch_size=20, epochs=100, verbose=1, validation_split=0.2)
+    history = cnn_model.fit(data_train_2d, target_train_oh, batch_size=20, epochs=CNN_EPOCH, verbose=1, validation_split=0.2)
 
     return cnn_model
 
@@ -89,20 +85,18 @@ def hidden_layer_generate(cnn_model):
     return hidden_layer_model, cnn_train_result
 
 
-def elm_model_generate(train_data):
+def elm_model_generate(data_train, target_train):
 
     """
     ELMモデルの構築
     """
 
-    target_train_oh = np_utils.to_categorical(target_test, NUM_CLASS)
+    target_train_oh = np_utils.to_categorical(target_train, NUM_CLASS)
 
     elm_model = hpelm.elm.ELM(cnn_train_result.shape[1], NUM_CLASS)
     elm_model.add_neurons(ELM_HIDDEN_NEURONS, func='sigm')
 
-    elm_model.train(train_data, target_train_oh, 'c')
-
-    # elm_model.confusion(target_train_oh, elm_model.predict(cnn_train_result))
+    elm_model.train(data_train, target_train_oh, 'c')
 
     return elm_model
 
@@ -124,12 +118,7 @@ def cnn_elm_evaluation(cnn_part, elm_part, data_test, target_test, file_name="pl
 
     elm_result_class = np.array([np.argmax(r) for r in elm_result])
 
-    confusion = elm_model.confusion(target_test_oh, elm_result_class)
-
-    # Convert one-hot to class
-    trace = plotly.graph_objs.Heatmap(z=confusion, colorscale=[[0, '#E6E6E6'], [1, '#04B486']])
-    plot_data = [trace]
-    plotly.offline.plot(plot_data, filename=file_name)
+    confusion = elm_model.confusion(target_test_oh, elm_result)
 
     # Confusion Matrix Plot
     trace = ff.create_annotated_heatmap(z=confusion, colorscale=[[0, '#E6E6E6'], [1, '#04B486']])
@@ -150,6 +139,6 @@ if __name__ == '__main__':
 
     hidden_layer_model, cnn_train_result = hidden_layer_generate(cnn_model)
 
-    elm_model = elm_model_generate(cnn_train_result)
+    elm_model = elm_model_generate(cnn_train_result, target_train)
 
     precision, recall, fscore, support = cnn_elm_evaluation(hidden_layer_model, elm_model, data_test_2d, target_test)
